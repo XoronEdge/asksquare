d05edf2e613c3ac4958672e115ece336d1a06f2f
xoronedge <ta.xit@yandex.com>
2020-10-10 15:27:23 +0500

    Remove  unwanted code

--- cmd/main.go
+++ cmd/main.go
@@ -9,19 +9,9 @@ import (
 	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	// _articleHttpDelivery "github.com/XoronEdge/quizzifire/internal/article/delivery/http"
-	// _articleHttpDeliveryMiddleware "github.com/XoronEdge/quizzifire/internal/article/delivery/http/middleware"
-	// _articleRepo "github.com/XoronEdge/quizzifire/internal/article/repository/mysql"
-	// _articleUcase "github.com/XoronEdge/quizzifire/internal/article/usecase"
-	// _authorRepo "github.com/XoronEdge/quizzifire/internal/author/repository/mysql"
 )
 
 func init() {
-	// err := godotenv.Load(".env")
-
-	// if err != nil {
-	// 	log.Fatalf("Error loading .env file")
-	// }
 	viper.AutomaticEnv()
 	if viper.GetBool(`debug`) {
 		log.Println("Service RUN on DEBUG mode")
@@ -42,22 +32,11 @@ func main() {
 		panic("failed to connect database")
 	}
 
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	// middL := _articleHttpDeliveryMiddleware.InitMiddleware()
-	// e.Use(middL.CORS)
-	// authorRepo := _authorRepo.NewMysqlAuthorRepository(dbConn)
-	// timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
-	// ar := _articleRepo.NewMysqlArticleRepository(dbConn)
-	// au := _articleUcase.NewArticleUsecase(ar, authorRepo, timeoutContext)
-	// _articleHttpDelivery.NewArticleHandler(e, au)
 	log.Fatal(e.Start(viper.GetString("ADDRESS")))
 
 }
