cb34d2f5b8575782d8f07a4099e338e9e518a6c9
xoronedge <ta.xit@yandex.com>
2020-10-20 20:21:15 +0500

    Add new fields in user table

--- db/migration/000001_user_schema.up.sql
+++ db/migration/000001_user_schema.up.sql
@@ -1,11 +1,18 @@
+CREATE TYPE genderType AS ENUM ('Male', 'Female', 'Other');
+
 CREATE TABLE "users" (
     "id" bigserial PRIMARY KEY,
     "username" VARCHAR(255) NOT NULL CONSTRAINT usernamechk CHECK (char_length(username) >= 5),
     "email" VARCHAR(255) NOT NULL CONSTRAINT emailchk CHECK (char_length(email) >= 5),
+    "first_name" VARCHAR(255)  CONSTRAINT firstnamechk CHECK (char_length(first_name) >= 5),
+    "last_name" VARCHAR(255)  CONSTRAINT lastnamechk CHECK (char_length(last_name) >= 5),
+    "phone" VARCHAR(255) NOT NULL CONSTRAINT phonechk CHECK (char_length(phone) >= 7),
+    "gender" genderType  CONSTRAINT genderchk CHECK(gender in ('Male', 'Female', 'Other')),
     "hash_password" VARCHAR(255) NOT NULL CONSTRAINT passwordchk CHECK (hash_password <> ''),
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT (now()),
     "updated_at" TIMESTAMPTZ,
     "deleted_at" TIMESTAMPTZ
 );
+
 CREATE UNIQUE INDEX idx_users_username ON users(username);
 CREATE UNIQUE INDEX idx_users_email ON users(email);
