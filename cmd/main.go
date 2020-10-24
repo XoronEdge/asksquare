8781ac153d281df40d5c060fd534c58f4d9f84e8
FahadKhan <fahad.khan@regbits.com>
2020-10-24 19:35:30 +0500

    Make project runnable

--- cmd/main.go
+++ cmd/main.go
@@ -11,39 +11,43 @@ import (
 	"github.com/XoronEdge/quizzifire/graph/generated"
 	userRepo "github.com/XoronEdge/quizzifire/internal/user/repo/postgres"
 	userUsecase "github.com/XoronEdge/quizzifire/internal/user/usecase"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 
 	"github.com/labstack/echo/v4"
 	"github.com/spf13/viper"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
-func init() {
-	viper.AutomaticEnv()
-	if viper.GetBool(`debug`) {
-		log.Println("Service RUN on DEBUG mode")
-	}
-}
+// func init() {
+// 	viper.AutomaticEnv()
+// 	if viper.GetBool(`debug`) {
+// 		log.Println("Service RUN on DEBUG mode")
+// 	}
+// }
 
 func main() {
-	dbHost := viper.GetString("DBHOST")
-	dbPort := viper.GetInt(`DBPORT`)
-	dbUser := viper.GetString(`DBUSER`)
-	dbPass := viper.GetString(`DBPASS`)
-	dbName := viper.GetString(`DBNAME`)
-	connection := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		dbHost, dbPort, dbUser, dbPass, dbName)
-	dbConn, err := gorm.Open(postgres.Open(connection), &gorm.Config{})
+	// dbHost := viper.GetString("DBHOST")
+	// dbPort := viper.GetInt(`DBPORT`)
+	// dbUser := viper.GetString(`DBUSER`)
+	// dbPass := viper.GetString(`DBPASS`)
+	// dbName := viper.GetString(`DBNAME`)
+	// connection := fmt.Sprintf("host=%s port=%d user=%s "+
+	// 	"password=%s dbname=%s sslmode=disable",
+	// 	dbHost, dbPort, dbUser, dbPass, dbName)
+	// fmt.Println(connection)
+	// dbConn, err := gorm.Open(postgres.Open(connection), &gorm.Config{})
+
+	dsn := "host=localhost user=postgres password=postgres dbname=quizzifire port=5432 sslmode=disable"
+	dbConn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
+		fmt.Println(err)
 		panic("failed to connect database")
 	}
-
 	e := echo.New()
 	ur := userRepo.NewUserRepo(dbConn)
 	uc := userUsecase.NewUserUsecase(ur, time.Minute)
 
-	graphqlHandler := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{Uc: &uc}}))
+	graphqlHandler := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{Uc: uc}}))
 	playgroundHandler := playground.Handler("GraphQL", "/query")
 
 	e.POST("/query", func(c echo.Context) error {
